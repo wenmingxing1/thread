be914592b15aaef076e68f1ff2dbc63d03467fee
wenmingxing1 <1793701192@qq.com>
2018-03-21 11:48:55 +0800

    两个线程交叉打印A和B

--- printABC.cpp
+++ printABC.cpp
@@ -1,31 +1,28 @@
 #include<iostream>
 #include<mutex>
 #include<thread>
-#include<condition_variable>
+//#include<condition_variable>
 #include<vector>
 
 using namespace std;
 
-std::mutex mtx;
-std::condition_variable cvar;
+std::mutex mtx;     //互斥量
+//std::condition_variable cvar;   //条件变量
 
 void myprint(char ch) {
     int nums = 10;
 
     for (int i = 0; i < nums; ++i) {
         std::unique_lock<std::mutex> ulk(mtx);
-        cout << ch << endl;
 
-        cvar.notify_one();
+        cout << this_thread::get_id() << " : "<< ch << endl;
+
+        //cvar.notify_one();
     }
 }
 
 int main() {
-    /*
-    vector<std::thread> threads;
-    threads.push_back(std::thread(myprint, 'A'));
-    threads.push_back(std::thread(myprint, 'B'));
-    */
+
     std::thread th1(myprint, 'A');
     std::thread th2(myprint, 'B');
 
